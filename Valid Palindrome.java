5f06233d680ca2084f8b8aaad84741d92abf00cc
Kevin Li <kevinli29033@gmail.com>
2019-10-28 12:58:03 -0400

    Added valid palindrome solution

---
+++ Valid Palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        int i = 0, j = s.length()-1;
+
+        while(i<j){
+            char a = Character.toLowerCase(s.charAt(i));
+            char b = Character.toLowerCase(s.charAt(j));
+
+            if(!Character.isLetterOrDigit(a)){
+                i++;
+            } else if(!Character.isLetterOrDigit(b)){
+                j--;
+            } else {
+                if(a!=b){
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+
+        return true;
+    }
+}
